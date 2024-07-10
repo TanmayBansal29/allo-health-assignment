02a2c92d8380b3319f1c8c1b4f21f5bd11e321ef
TanmayBansal29 <bansaltanmay554@gmail.com>
2024-07-10 20:59:58 +0530

    Basic Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux"
 import { Route, Routes } from "react-router-dom"
 import { store } from "../src/redux/store/store"
-import PrivateRoutes from "./PrivateRoutes"
+import PrivateRoutes from "./components/PrivateRoutes"
 import Home from "./pages/Home"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
