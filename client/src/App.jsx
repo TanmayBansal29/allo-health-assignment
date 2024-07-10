cb398771f8f2ef9ceb63cf6857ec51c23cfd8a14
TanmayBansal29 <bansaltanmay554@gmail.com>
2024-07-10 21:08:09 +0530

    Final One

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux"
 import { Route, Routes } from "react-router-dom"
 import { store } from "../src/redux/store/store"
-import PrivateRoutes from "./components/PrivateRoutes"
 import Home from "./pages/Home"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
@@ -11,9 +10,7 @@ function App() {
   return(
     <Provider store={store}>
     <Routes>
-      <Route element={<PrivateRoutes/>}>
-        <Route path="/meals" element={<Home/>}/>
-      </Route>
+      <Route path="/meals" element={<Home/>}/>
       <Route path="/signup" element={<SignUpPage/>}/>
       <Route path="/" element={<LoginPage/>}/>
     </Routes>
